6d971a1a668ce5024cc3f6bb4005783ba01f0703
n-arms <75282394+n-arms@users.noreply.github.com>
2024-03-22 20:47:14 -0400

    Change back to using directory deployment

--- src/main/java/astrolabe/generate/Main.java
+++ src/main/java/astrolabe/generate/Main.java
@@ -14,7 +14,8 @@ public class Main {
     public static void main(String[] args) throws Exception {
         String name = System.getenv("TO_DEPLOY");
 
-        String pathName = "./src/main/deploy" + "/pathplanner/paths/" + name + ".path";
+        //String pathName = "./src/main/deploy" + "/pathplanner/paths/" + name + ".path";
+        String pathName = Filesystem.getDeployDirectory() + "/pathplanner/paths/" + name + ".path";
 
         AstrolabePath path = PathParser.loadPath(pathName);
         XYSpline spline = Heuristic.fromPath(path);
