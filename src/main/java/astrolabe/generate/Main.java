80d2e57a9d5e68b5560e2abc48e1bf4689136cfa
n-arms <75282394+n-arms@users.noreply.github.com>
2024-03-22 20:19:32 -0400

    this isn't a sketchy workaround, you're a sketchy workaround, why are you being so sketchy

--- src/main/java/astrolabe/generate/Main.java
+++ src/main/java/astrolabe/generate/Main.java
@@ -14,7 +14,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         String name = new Scanner(System.in).nextLine();
 
-        String pathName = Filesystem.getDeployDirectory() + "/pathplanner/paths" + name + ".path";
+        String pathName = "./src/main/deploy" + "/pathplanner/paths" + name + ".path";
 
         AstrolabePath path = PathParser.loadPath(pathName);
         XYSpline spline = Heuristic.fromPath(path);
