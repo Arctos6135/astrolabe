0faf6fae2fab6d75947bcff1efad592c46e995a2
n-arms <75282394+n-arms@users.noreply.github.com>
2024-03-24 12:37:59 -0400

    Alter trajectory profiling to more accurately represent robot constraints

--- src/main/java/astrolabe/generate/spline/Profiler.java
+++ src/main/java/astrolabe/generate/spline/Profiler.java
@@ -13,11 +13,13 @@ public class Profiler {
         public double time;
         public double distance;
         public double velocity;
+        public double maxAcceleration;
+        public double minAcceleration;
         public double acceleration;
 
         @Override
         public String toString() {
-            return String.format("ConstrainedState [distance=%.2f, velocity=%.2f, acceleration=%.2f]", distance, velocity, acceleration);
+            return String.format("ConstrainedState [distance=%.2f, velocity=%.2f, max acceleration=%.2f, min acceleration=%.2f]", distance, velocity, maxAcceleration, minAcceleration);
         }
     }
     private record State(
@@ -37,7 +39,7 @@ public class Profiler {
 
         double t;
 
-        for (t = 0; t <= 1; t += 0.001) {
+        for (t = 0; t <= 1; t += 0.1) {
             double x = xSpline.sample(t);
             double y = ySpline.sample(t);
             // very sketchy way to find the arc length from the last point
@@ -70,7 +72,7 @@ public class Profiler {
 
         return new State(new Pose2d(x, y, Rotation2d.fromRadians(Math.atan2(dy, dx))), curvature);
     }
-
+/*
     private static Trajectory profileTime(
         List<State> states,
         double maxVelocity,
@@ -80,54 +82,66 @@ public class Profiler {
     ) {
         ArrayList<ConstrainedState> constrained = new ArrayList<>();
 
+        
         ConstrainedState previous = new ConstrainedState();
         previous.velocity = startVelocity;
         previous.acceleration = maxAcceleration;
         previous.state = states.get(0);
         constrained.add(previous);
+    
 
-        // Forward pass
+        // Initial pass
         for (int i = 1; i < states.size(); i++) {
             ConstrainedState current = new ConstrainedState();
             current.state = states.get(i);
 
             current.distance = current.state.pose.getTranslation().getDistance(previous.state.pose.getTranslation());
 
-
-            // enforce positive acceleration constraints
-            current.velocity = Math.min(
-                maxVelocity,
-                Math.sqrt(
-                    previous.velocity * previous.velocity + previous.acceleration * current.distance * 2
-                )
-            );
-
-            current.acceleration = maxAcceleration;
-
-
-            previous = current;
+            current.velocity = maxVelocity;
 
             constrained.add(current);
         }
 
-        ConstrainedState next = new ConstrainedState();
-        next.velocity = endVelocity;
-        next.state = states.get(states.size() - 1);
+        for (int iteration = 0; iteration < 100; iteration++) {
+            constrained.get(0).velocity = startVelocity;
+            previous = constrained.get(0);
 
-        // Backward pass
-        for (int i = constrained.size() - 1; i >= 0; i--) {
-            ConstrainedState current = constrained.get(i);
+            // Forward pass
+            for (int i = 1; i < states.size(); i++) {
+                ConstrainedState current = constrained.get(i);
 
-            current.velocity = Math.min(
-                current.velocity,
-                Math.sqrt(
-                    next.velocity * next.velocity + maxAcceleration * current.distance * 2.0
-                )
-            );
+                // enforce positive acceleration constraints
+                current.velocity = Math.min(
+                    constrained.get(i).velocity,
+                    Math.sqrt(
+                        previous.velocity * previous.velocity + previous.acceleration * current.distance * 2
+                    )
+                );
 
-            next = current;
+                current.acceleration = maxAcceleration;
+
+                previous = current;
+            }
+
+            ConstrainedState next = constrained.get(constrained.size() - 1);
+            constrained.get(constrained.size() - 1).velocity = endVelocity;
+
+            // Backward pass
+            for (int i = constrained.size() - 1; i >= 0; i--) {
+                ConstrainedState current = constrained.get(i);
+
+                current.velocity = Math.min(
+                    current.velocity,
+                    Math.sqrt(
+                        next.velocity * next.velocity + maxAcceleration * current.distance * 2.0
+                    )
+                );
+
+                next = current;
+            }
         }
 
+        
         List<Trajectory.State> trajectory = new ArrayList<>();
         double time = 0;
         double distance = 0;
@@ -151,8 +165,11 @@ public class Profiler {
             if (trajectory.size() != 0) {
                 trajectory.get(trajectory.size() - 1).accelerationMetersPerSecondSq = acceleration;
 
-                //dt = (current.velocity - velocity) / acceleration;
-                dt = ds / current.velocity;
+                if (acceleration == 0) {
+                    dt = ds / current.velocity;
+                } else {
+                    dt = (current.velocity - velocity) / acceleration;
+                }
             }
 
             if (dt < 0) {
@@ -175,7 +192,121 @@ public class Profiler {
 
         return new Trajectory(trajectory);
     }
+*/
+    private static Trajectory profileTime(
+        List<State> states,
+        double maxVelocity,
+        double maxAcceleration,
+        double startVelocity,
+        double endVelocity
+    ) {
+        ArrayList<ConstrainedState> constrained = new ArrayList<>();
 
+        double epsilon = 1e-6;
+
+        {
+            Pose2d previous = states.get(0).pose;
+            for (State state : states) {
+                ConstrainedState inner = new ConstrainedState();
+                inner.distance = state.pose.getTranslation().getDistance(previous.getTranslation());
+                inner.state = state;
+                inner.velocity = maxVelocity;
+                inner.maxAcceleration = maxAcceleration;
+                inner.minAcceleration = -maxAcceleration;
+
+                previous = inner.state.pose;
+                constrained.add(inner);
+            }
+        }
+
+        System.out.println("After initialization:");
+        for (ConstrainedState constrainedState : constrained) {
+            System.out.println(constrainedState.toString());
+        }
+
+        {
+            ConstrainedState previous = constrained.get(0);
+            previous.velocity = startVelocity;
+
+            for (int i = 1; i < constrained.size(); i++) {
+                ConstrainedState current = constrained.get(i);
+
+                current.velocity = Math.min(
+                    current.velocity,
+                    Math.sqrt(previous.velocity * previous.velocity + 2 * previous.maxAcceleration * current.distance)
+                );
+
+                previous = current;
+            }
+        }
+
+        System.out.println("After fist pass:");
+        for (ConstrainedState constrainedState : constrained) {
+            System.out.println(constrainedState.toString());
+        }
+
+        {
+            ConstrainedState next = constrained.get(constrained.size() - 1);
+            next.velocity = endVelocity;
+            next.minAcceleration = -maxAcceleration;
+            next.maxAcceleration = maxAcceleration;
+
+            for (int i = constrained.size() - 2; i >= 0; i--) {
+                ConstrainedState current = constrained.get(i);
+
+                current.velocity = Math.min(
+                    current.velocity, 
+                    Math.sqrt(next.velocity * next.velocity - 2 * next.minAcceleration * next.distance)
+                );
+
+                next = current;
+            }
+        }
+
+        System.out.println("After second pass:");
+        for (ConstrainedState constrainedState : constrained) {
+            System.out.println(constrainedState.toString());
+        }
+
+        double time = 0;
+
+        for (int i = 0; i < constrained.size() - 1; i++) {
+            ConstrainedState current = constrained.get(i);
+            ConstrainedState next = constrained.get(i + 1);
+            if (Math.abs(next.distance) < epsilon) {
+                throw new RuntimeException("Unacceptable distance from previous state in state " + next.toString() + " at index " + i);
+            }
+            current.time = time;
+
+            double acceleration = (next.velocity * next.velocity - current.velocity * current.velocity) / (2 * next.distance);
+
+            double dt = 0;
+
+            if (Math.abs(acceleration) > epsilon) {
+                dt = (next.velocity - current.velocity) / acceleration;
+            } else if (Math.abs(current.velocity) > epsilon) {
+                dt = next.distance / current.velocity;
+            } else {
+                throw new RuntimeException("Both acceleration and velocity are 0 at state " + current.toString() + " at index " + i);
+            }
+
+            time += dt;
+
+            current.acceleration = acceleration;
+        }
+
+        ConstrainedState last = constrained.get(constrained.size() - 1);
+        last.time = time;
+        last.acceleration = 0;
+
+        List<Trajectory.State> trajectory = new ArrayList<>();
+
+        for (ConstrainedState state : constrained) {
+            trajectory.add(new Trajectory.State(state.time, state.velocity, state.acceleration, state.state.pose, state.state.curvature));
+        }
+
+        return new Trajectory(trajectory);
+    }
     public static Trajectory fromSpline(
         Spline x,
         Spline y,
